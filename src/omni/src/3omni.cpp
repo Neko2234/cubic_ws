a93d7bfe5879a92789953294b1a8950b7463117c
Neko2234 <biwakocatfish@gmail.com>
2022-11-25 14:45:08 +0900

    get data from encodor

--- src/omni/src/3omni.cpp
+++ src/omni/src/3omni.cpp
@@ -2,6 +2,7 @@
 #include <std_msgs/Int16MultiArray.h>
 #include <sensor_msgs/Joy.h>
 #include <string>
+#include <time.h>
 
 #define MOTOR_NUM 3
 #define PW_MAX 100 //モータ出力絶対値の最大値
@@ -14,15 +15,44 @@ std_msgs::Int16MultiArray order;
 
 ros::Publisher pub;
 ros::Subscriber sub;
+ros::Subscriber enc_sub;
 
 double vx = 0, vy = 0, ang = 0;
 double v_raw[MOTOR_NUM] = {0};
 int16_t v[MOTOR_NUM] = {0};
+int e_raw[MOTOR_NUM] = {0};
+double t;
+double s_raw[MOTOR_NUM] = {0};
+int16_t s[MOTOR_NUM] = {0};
 
 void callback(const sensor_msgs::Joy::ConstPtr& msg) {
     //-1.0 <= vx,vy <= 1.0
     vx = -msg->axes[0], vy = msg->axes[1], ang = -msg->axes[2];
+}
+
+void encCb(const std_msgs::Int16MultiArray& enc_msg){
+	e_raw[0] = enc_msg.data[0];
+	e_raw[1] = enc_msg.data[1];
+	e_raw[2] = enc_msg.data[2];
+}
+
+void target_culc(){
+	for(int i=0; i<MOTOR_NUM; i++){
+		s_raw[i] = e_raw[i]/t;//時間あたりのパルス
+	}
+	
+    int s_sum=1;
+    s_sum = (s_raw[0] + s_raw[1] + s_raw[2]);
     
+    if(s_sum != 0){
+        for(int i; i<MOTOR_NUM; i++){
+	    	s[i] = (int)((s_raw[i] / s_sum)*100);
+        }
+    }else if(s_sum == 0){//エラー
+        s[0] = s_raw[0];
+        s[1] = 39;
+        s[2] = 39;
+    }
 }
 
 void omni_calc() {
@@ -49,7 +79,8 @@ void omni_calc() {
 void publish() {
     //order.data.resize(4);
     for (int i = 0; i < MOTOR_NUM; i++) {
-        order.data[i] = v[i];
+	order.data[i] = s[i];
+        //order.data[i] = v[i];
     }
     pub.publish(order);
 }
@@ -62,15 +93,20 @@ int main(int argc, char **argv) {
 
     pub = nh.advertise<std_msgs::Int16MultiArray>("omni", 1);
     sub = nh.subscribe("joy", 1, callback);
+    enc_sub = nh.subscribe("encoder", 1, encCb);
 
     ros::Rate loop_rate(10);
     while(ros::ok()) {
+    	clock_t start = clock();    // スタート時間
         ros::spinOnce();
-
+	
+	target_culc();
         omni_calc();
 
         publish();
 
         loop_rate.sleep();
+        clock_t end = clock();     // 終了時間
+        t = (double)start - end;
     }
 }
